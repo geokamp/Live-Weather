f25fee7a2ee0d851e8efbcda8085889132e06d98
George Kampouris <geokamp98@gmail.com>
2025-01-17 11:19:05 +0200

    1st

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,75 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import  React,  { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+const [city, setCity] = useState('');
+const [data, setData] = useState([]);
+
+
+const convertKelvinToCelsius = (kelvin) => {
+  return (kelvin - 273.15).toFixed(2);
+};
+
+
+
+const handleClick = (event) => {
+  if (event.key === 'Enter') {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ec1c08fdbd8c0694bf55fb529d582507`)
+      .then(response => response.json())
+      .then(data => {
+          setData(data); 
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+};
+
+
+
+
+
+
+
+
+  return(
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      height: '100vh',
+      flexDirection: 'column',
+      paddingTop: '20px',
+    }}>
+      <div style={{display: 'flex',alignItems:"center"}}>
+        <TextField id="outlined-basic" label="Search" value={city} variant="outlined" onChange={(e) => setCity(e.target.value) } onKeyDown={handleClick} />
+        <SearchIcon fontSize="large"  />
+      </div>
+
+      <div style={{display:'flex', flexDirection:'column' }}>
+        
+        <div style={{padding:"15px"}}>
+        {data ? (
+          <div style={{background: "linear-gradient(to right,rgba(99, 99, 99, 0.35),rgba(94, 94, 94, 0.35),rgb(97, 97, 97, 0.35),rgb(88, 88, 88, 0.35))", borderBlockStyle: "none", borderRadius:"10px", padding:"5px"}}>
+            <h1>Weather Info for {data.name}</h1>
+            <h3>Temperature: {data.main ? convertKelvinToCelsius(data.main.temp) : 'N/A'}°C</h3>
+            <h4>Feels like: {data.main ? convertKelvinToCelsius(data.main.feels_like) : 'N/A'}°C</h4>
+            <h3>Weather: {data.weather && data.weather[0] ? data.weather[0].description : 'N/A'}</h3>
+            <p></p>
+          </div>
+        ): (
+          <p>Loading data...</p>
+        )}
+          
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      
     </div>
-  );
+  )
 }
